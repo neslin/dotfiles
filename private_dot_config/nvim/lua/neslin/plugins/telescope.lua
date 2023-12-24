c216b658c9b1d4926d679aa0db52fa003461f94e
neslin <christian@neslin.se>
2023-12-24 20:39:58 +0100

    Moar plugins

--- private_dot_config/nvim/lua/neslin/plugins/telescope.lua
+++ private_dot_config/nvim/lua/neslin/plugins/telescope.lua
@@ -2,11 +2,35 @@ return {
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.5',
         lazy = false,
-        -- or                              , branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        keys = {
-            { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
-            { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Telescope buffers" },
+        dependencies = { 
+            "nvim-lua/plenary.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+            "nvim-tree/nvim-web-devicons",
         },
+        config = function()
+            local telescope = require("telescope")
+            local actions = require("telescope.actions")
+
+            telescope.setup({
+                defaults = {
+                    path_display = { "truncate " },
+                    mappings = {
+                        i = {
+                            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                            ["<C-j>"] = actions.move_selection_next, -- move to next result
+                            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                        }
+                    }
+                }
+            })
+
+            telescope.load_extension("fzf")
+            local keymap = vim.keymap
+
+            keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Telescope find files" })
+            keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Telescope buffers" })
+            keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+            keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+        end,
     }
 }
--- private_dot_config/nvim/lua/neslin/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/neslin/plugins/treesitter.lua
@@ -1,6 +1,10 @@
 return{
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    even = { "BufReadPre", "BufNewFile"},
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
     config = function ()
         local configs = require("nvim-treesitter.configs")
 
@@ -9,7 +13,16 @@ return{
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
-            ignore_install = { "help" }
+            ignore_install = { "help" },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<BS>",
+                }
+            }
         })
     end
 }
