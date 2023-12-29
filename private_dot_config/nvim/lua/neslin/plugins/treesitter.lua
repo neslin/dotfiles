87a3de7f16f4db793c87555d3cffb16cc22cf833
neslin <christian@neslin.se>
2023-12-29 03:37:53 +0100

    Don't remember

--- private_dot_config/nvim/lua/neslin/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/neslin/plugins/treesitter.lua
@@ -1,28 +1,29 @@
-return{
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    even = { "BufReadPre", "BufNewFile"},
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
-    config = function ()
-        local configs = require("nvim-treesitter.configs")
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	even = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "python" },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-            ignore_install = { "help" },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-space>",
-                    node_incremental = "<C-space>",
-                    scope_incremental = false,
-                    node_decremental = "<BS>",
-                }
-            }
-        })
-    end
+		configs.setup({
+			--ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "python" },
+			auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+			ignore_install = { "help" },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<BS>",
+				},
+			},
+		})
+	end,
 }
