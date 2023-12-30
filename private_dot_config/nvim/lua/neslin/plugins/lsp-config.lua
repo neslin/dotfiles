f394b99e59dc6641426e5afd7aadf8fdd404f286
neslin <christian@neslin.se>
2023-12-30 03:42:42 +0100

    Remove deleted stuff

--- private_dot_config/nvim/lua/neslin/plugins/lsp-config.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require "neslin.lspconfig"
-            require "neslin.plugins.lsp-config"
-        end,
-    },
-}
