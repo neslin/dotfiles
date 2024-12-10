bf4f7fbbdf265b33d1e31b549e66ee3c57b29536
neslin.andromeda <christian@neslin.se>
2024-12-10 20:01:50 +0100

    Nvim plugin fixes

--- private_dot_config/nvim/lua/neslin/plugins/telescope.lua
+++ private_dot_config/nvim/lua/neslin/plugins/telescope.lua
@@ -1,37 +1,31 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
-        lazy = false,
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-            "nvim-tree/nvim-web-devicons",
-        },
-        config = function()
-            local telescope = require("telescope")
-            local actions = require("telescope.actions")
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    -- or                              , branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local telescope = require("telescope")
+        local actions = require("telescope.actions")
 
-            telescope.setup({
-                defaults = {
-                    path_display = { "truncate " },
-                    mappings = {
-                        i = {
-                            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-                            ["<C-j>"] = actions.move_selection_next,     -- move to next result
-                            ["<leader>fq"] = actions.send_selected_to_qflist + actions.open_qflist,
-                        }
+        local builtin = require('telescope.builtin')
+        local keymap = vim.keymap
+
+        telescope.setup({
+            defaults = {
+                path_display = { "truncate " },
+                mappings = {
+                    i = {
+                        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                        ["<C-j>"] = actions.move_selection_next,     -- move to next result
+                        ["<leader>fq"] = actions.send_selected_to_qflist + actions.open_qflist,
                     }
                 }
-            })
+            }
+        })
 
-            telescope.load_extension("fzf")
-            local keymap = vim.keymap
-
-            keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Telescope find files" })
-            keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Telescope buffers" })
-            keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-            keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-        end,
-    }
+        keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    end,
 }
